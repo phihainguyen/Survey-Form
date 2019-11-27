68ee3b8d83f7b1c78e76f066f951596a688ec8d7
Phi Hai Nguyen <phihai93@gmail.com>
2019-11-26 16:30:06 -0800

    mysql

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const app = express();
-app.listen(3000, () => console.log("listening at 3000"));
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "survey_db"
+});
+connection.connect(error => {
+  if (error) {
+    console.error("hello there is error " + error);
+  } else {
+    console.log("successfully connected to DB");
+  }
+});
 app.use(express.static("../public"));
+
+app.listen(3000, () => console.log("listening at 3000"));
