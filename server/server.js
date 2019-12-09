39e54d9b8150a9518ff6d71f3bab7b125298e9a5
Phi Hai Nguyen <phihai93@gmail.com>
2019-12-08 20:49:24 -0800

    update routes

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const apiRroutes = require("./routes");
+
+app.use(express.json());
+//allowing us to use the routes in the index.js file inside routes folder
+app.use("/api/ex", apiRroutes);
+app.listen(process.env.PORT || "3000", () => {
+  console.log(`listening on port ${process.env.PORT || "3000"}`);
+});
+//this should work
